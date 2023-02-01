b62cd0824ac9272ff6d8846ff82b4d273913a0a0
JacyF <brunoloouuiis@gmail.com>
2023-02-02 00:10:18 +0300

    added exe001 and exe002

--- Python basic (Part -I)/exe001.py
+++ Python basic (Part -I)/exe001.py
@@ -10,3 +10,17 @@
 # 		Like a diamond in the sky. 
 # Twinkle, twinkle, little star, 
 # 	How I wonder what you are
+
+world = ['Twinkle, twinkle, little star',
+         'How I wonder what you are!',
+         'Up above the world so high,',
+         'Like a diamond in the sky.',
+         'Twinkle, twinkle, little star,',
+         'How I wonder what you are']
+
+print(f"--> {world[0]}")
+print(f"--> {world[1]}")
+print(f"--> {world[2]}")
+print(f"--> {world[3]}")
+print(f"--> {world[4]}")
+print(f"--> {world[5]}")
---
+++ Python basic (Part -I)/exe002.py
@@ -0,0 +1,9 @@
+# 2. Write a Python program to find out what version of Python you are using. 
+
+import sys
+print("Python Version : ")
+print(sys.version)
+
+print("Version Information : ")
+print(sys.version_info)
+
