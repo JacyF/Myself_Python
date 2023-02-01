838224c21393686f1e9a38f5a49de3a8abe1bd38
JacyF <brunoloouuiis@gmail.com>
2023-02-02 00:49:14 +0300

    added exe005

---
+++ Python basic (Part -I)/exe005.py
@@ -0,0 +1,9 @@
+# 5. Write a Python program that accepts the user's first and last name 
+# and prints them in reverse order with a space between them. 
+
+firstname = str(input("Please write your first name : "))
+lastname = str(input("Please write your last name : "))
+
+print(f"The print of entered name in reverse form looks like that --> {lastname}  {firstname}")
+
+
